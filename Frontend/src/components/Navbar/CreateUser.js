f682d63c6a30caa7f0661e9d7fd5fe4a20357b2b
arnav <arnav.kumar735@gmail.com>
2021-11-17 11:26:08 +0530

    navbar updated

--- Frontend/src/components/Navbar/CreateUser.js
+++ Frontend/src/components/Navbar/CreateUser.js
@@ -1,32 +1,17 @@
-import { React, useState, useEffect } from "react";
+import { React, useState } from "react";
 import { isEmpty, isEmail } from "validator";
 
 export default function CreateUser(props) {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const [id, setId] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [emailId, setEmailId] = useState("");
   const [city, setCity] = useState("");
   const [state, setState] = useState("");
   const [country, setCountry] = useState("");
-  // const [userCreatedTime, setUserCreatedTime] = useState("");
-  // const [loginTime, setLoginTime] = useState("");
-  // const [lastLogOffTime, setLastLogOffTime] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
   const [rolesToUser, setRolesToUser] = useState([]);
-
-  // const [rolesToUser, setRolesToUser] = useState("");
-
-  const [passIcon, setPassIcon] = useState(false);
-  // const [isEmpty, setIsEmpty] = useState(false);
-  // const [isError, setIsError] = useState(false);
-  const [errors, setErrors] = useState({
-    isError: false,
-    isEmpty: false,
-  });
   const [error, setError] = useState(false);
-
   const [inputErrors, setInputErrors] = useState({
     firstName: "",
     lastName: "",
@@ -37,145 +22,93 @@ export default function CreateUser(props) {
   });
 
   const handleCheckBox = (event) => {
-
-    console.log(event.target.checked);
     let isChecked = event.target.checked;
     let selectedRole = event.target.value;
 
     if (isChecked) {
       setRolesToUser((prevState) => [...prevState, { roleName: selectedRole }]);
-
     } else {
       let newRoles = rolesToUser.filter(
-        (role) => role.roleName != selectedRole
+        (role) => role.roleName !== selectedRole
       );
       setRolesToUser(newRoles);
     }
-    console.log(selectedRole);
   };
-  const handleCloseBtn = () => {
-    // setIsError(false);
-    // setIsEmpty(false);
-    setErrors({
-      isError: false,
-      isEmpty: false,
+
+  const handleInputError = (fieldName, msg) => {
+    setInputErrors((prevState) => {
+      return { ...prevState, [fieldName]: msg };
     });
   };
 
   const validateInputs = () => {
     let formError = false;
-    
 
-    if (!isEmpty(firstName)) { 
-      setInputErrors((prevState) => {
-        return { ...prevState, firstName: "" };
-      })
+    if (!isEmpty(firstName)) {
+      handleInputError("firstName", "");
     } else {
-      setInputErrors((prevState) => {
-        return { ...prevState, firstName: "Enter FirstName" };
-      });
-      console.log("Enter firstName");
+      handleInputError("firstName", "Enter fistName");
       formError = true;
     }
 
     if (!isEmpty(lastName)) {
-      setInputErrors((prevState) => {
-        return { ...prevState, lastName: "" };
-      })
+      handleInputError("lastName", "");
     } else {
-      setInputErrors((prevState) => {
-        return { ...prevState, lastName: "Enter LastName" };
-      });
-      console.log("Enter lastName");
+      handleInputError("lastName", "Enter lastName");
       formError = true;
     }
 
     if (!isEmpty(emailId)) {
-      setInputErrors((prevState) => {
-        return { ...prevState, emailId: "" };
-      })
+      handleInputError("emailId", "");
       if (isEmail(emailId)) {
-        setInputErrors((prevState) => {
-          return { ...prevState, emailId: "" };
-        })
+        handleInputError("");
       } else {
-        setInputErrors((prevState) => {
-          return { ...prevState, emailId: "Enter a valid EmailId" };
-        });
-        console.log("Enter Valid emailID");
+        handleInputError("emailId", "Enter Valid Email Address");
         formError = true;
       }
     } else {
-      setInputErrors((prevState) => {
-        return { ...prevState, emailId: "Enter EmailId" };
-      });
-      console.log("enter emailId");
+      handleInputError("emailId", "Enter Email Id");
       formError = true;
     }
 
     if (!isEmpty(userName)) {
-      setInputErrors((prevState) => {
-        return { ...prevState, userName: "" };
-      })
+      handleInputError("userName", "");
       if (userName.match(/^[a-zA-Z]+[!@#$%*]?[0-9]+$/)) {
-        setInputErrors((prevState) => {
-          return { ...prevState, userName: "" };
-        })
+        handleInputError("userName", "");
       } else {
-        setInputErrors((prevState) => {
-          return { ...prevState, userName: "Check UserName Policy" };
-        });
-        console.log("Check username policy");
+        handleInputError("userName", "Check username policy");
         formError = true;
       }
     } else {
-      setInputErrors((prevState) => {
-        return { ...prevState, userName: "Enter UserName" };
-      });
-      console.log("Enter username");
+      handleInputError("userName", "Enter username");
       formError = true;
     }
 
     if (!isEmpty(password)) {
-      setInputErrors(prevState=>{return {...prevState, password : ""}});
-      
+      handleInputError("password", "");
       if (password.match(/^[A-Z]{1,2}[a-z]+[%+_!~-]{1,2}[0-9]+$/)) {
-        setInputErrors(prevState=>{return {...prevState, password : ""}});
-        
+        handleInputError("password", "");
       } else {
-        setInputErrors(prevState=>{return {...prevState, password : "Check Password Policy"}});
-        console.log("check password policy");
+        handleInputError("password", "Check password policy");
         formError = true;
       }
     } else {
-      setInputErrors(prevState=>{return {...prevState, password : "Enter Password"}});
-      console.log("enter password");
+      handleInputError("password", "Enter password");
       formError = true;
     }
 
-    if(rolesToUser.length===0){
-      setInputErrors(prevState=>{return {...prevState, rolesToUser : "Please Select roles"}});
+    if (rolesToUser.length === 0) {
+      handleInputError("rolesToUser", "Please select Roles");
+      formError = true;
+    } else {
+      handleInputError("rolesToUser", "");
     }
-    else{
-      setInputErrors(prevState=>{return {...prevState, rolesToUser : ""}});
-    }
+
     return formError;
   };
 
   const handleSubmit = async (event) => {
-    validateInputs();
-    console.log("errorrrrrrrrrrr");
     event.preventDefault();
-    // setIsEmpty(false);
-    // setIsError(false);
-    console.log(rolesToUser);
-    console.log(password);
-
-    setErrors({
-      isError: false,
-      isEmpty: false,
-    });
-    
 
     if (!validateInputs()) {
       let credentials = {
@@ -190,14 +123,6 @@ export default function CreateUser(props) {
         rolesToUser,
       };
 
-      // var formBody = [];
-
-      // for (var property in credentials) {
-      //   var encodedKey = encodeURIComponent(property);
-      //   var encodedValue = encodeURIComponent(credentials[property]);
-      //   formBody.push(encodedKey + "=" + encodedValue);
-      // }
-      // formBody = formBody.join("&");
       try {
         const data = await fetch(
           "http://localhost:9001/api/v1/auth/user/save",
@@ -206,13 +131,13 @@ export default function CreateUser(props) {
             headers: {
               Accept: "application/json",
               Authorization:
-                "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuYW5kaGEwODkyIiwicm9sZXMiOlsiUk9MRV9BRE1JTiJdLCJpc3MiOiJBVVRIX1NFUlZJQ0UiLCJleHAiOjE2MzcwNjUwMzQsImlhdCI6MTYzNzA2MTQzNH0.StZgSsI-H1m6xt7OPFsvUOsa8HccqAMwEChr8P_sq9w",
+                "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuYW5kaHUzMjQ1Iiwicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfVVNFUiJdLCJpc3MiOiJBVVRIX1NFUlZJQ0UiLCJleHAiOjE2MzcwNzM4MjksImlhdCI6MTYzNzA3MjAyOX0.XJlnF-FvYb8K6L8CZO71eInl9DjxQFW0jpy-fQ4AnZc",
               "Content-Type": "application/json",
             },
             body: JSON.stringify(credentials),
           }
         );
-        console.log(data);
+
         const result = await data.json();
         console.log("Success:", result);
         if (
@@ -221,36 +146,21 @@ export default function CreateUser(props) {
           result.hasOwnProperty("refresh_token")
         ) {
           props.history.push({ pathname: "/" });
-        } else if (
-          data.status === 400 &&
-          result.errorMessage === "bad credentials"
-        ) {
-          //setIsError(true);
-          setErrors((prevState) => {
-            return { ...prevState, isError: true };
-          });
+        } else if (data.status === 302) {
+          if (result.errorMessage === "username already exist") {
+            handleInputError("userName", "Username already taken");
+          } else if (result.errorMessage === "emailId already exist") {
+            handleInputError("emailId", "emailId already exist");
+          }
+          setError(true);
         }
       } catch (error) {
         console.error("Error:", error);
       }
     } else {
-      // setIsError(true);
-      // setIsEmpty(true);
       setError(true);
-      setErrors({
-        isError: true,
-        isEmpty: true,
-      });
     }
   };
-
-  useEffect(() => {
-    let clearIntervalId;
-    if (errors.isError) {
-      clearIntervalId = setTimeout(handleCloseBtn, 4000);
-    }
-    return () => clearInterval(clearIntervalId);
-  }, [errors.isError]);
 
   return (
     <div>
@@ -263,9 +173,16 @@ export default function CreateUser(props) {
               name="FirstName"
               value={firstName}
               placeholder="firstname"
-              onChange={(e) => setFirstName(e.target.value)}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
             ></input>
-            {error && <p>{inputErrors.firstName}</p> }
+            {error &&
+              (inputErrors.firstName.length > 0 ? (
+                <p>{inputErrors.firstName}</p>
+              ) : (
+                ""
+              ))}
             <input
               type="text"
               name="LastName"
@@ -273,7 +190,12 @@ export default function CreateUser(props) {
               placeholder="lastname"
               onChange={(e) => setLastName(e.target.value)}
             ></input>
-            {error && <p>{inputErrors.lastName}</p> }
+            {error &&
+              (inputErrors.lastName.length > 0 ? (
+                <p>{inputErrors.lastName}</p>
+              ) : (
+                ""
+              ))}
             <input
               type="email"
               name="EmailId"
@@ -281,7 +203,12 @@ export default function CreateUser(props) {
               placeholder="emailid"
               onChange={(e) => setEmailId(e.target.value)}
             ></input>
-            {error && <p>{inputErrors.emailId}</p> }
+            {error &&
+              (inputErrors.emailId.length > 0 ? (
+                <p>{inputErrors.emailId}</p>
+              ) : (
+                ""
+              ))}
             <input
               type="text"
               name="City"
@@ -302,7 +229,8 @@ export default function CreateUser(props) {
               value={country}
               placeholder="country"
               onChange={(e) => setCountry(e.target.value)}
-            ></input><br></br>
+            ></input>
+            <br></br>
             <input
               type="text"
               name="UserName"
@@ -310,27 +238,30 @@ export default function CreateUser(props) {
               placeholder="username"
               onChange={(e) => setUserName(e.target.value)}
             ></input>
-            {error && <p>{inputErrors.userName}</p> }
+            {error &&
+              (inputErrors.userName.length > 0 ? (
+                <p>{inputErrors.userName}</p>
+              ) : (
+                ""
+              ))}
             <input
-              type={!passIcon ? "password" : "text"}
+              type="password"
               name="Password"
               value={password}
               placeholder="password"
               onChange={(e) => setPassword(e.target.value)}
             ></input>
-            {error && <p>{inputErrors.password}</p>}
-            {password.length > 0 && (
-              <i onClick={() => setPassIcon((prevState) => !prevState)}></i>
-            )}
-            
-            <br></br>
+            {error &&
+              (inputErrors.password.length > 0 ? (
+                <p>{inputErrors.password}</p>
+              ) : (
+                ""
+              ))}
             <input
               type="checkbox"
               name="ROLE_ADMIN"
               id="admin"
               value="admin"
-              // placeholder="username"
-              //onChange={(e) => setRoles(e.target.value)}
               onClick={handleCheckBox}
             ></input>
             Admin
@@ -339,16 +270,18 @@ export default function CreateUser(props) {
               name="ROLE_USER"
               id="user"
               value="user"
-              // placeholder="username"
-              //onChange={(e) => setRoles(e.target.value)}
               onClick={handleCheckBox}
             ></input>
             User
-            {error && <p>{inputErrors.rolesToUser}</p>}
+            {error &&
+              (inputErrors.rolesToUser.length > 0 ? (
+                <p>{inputErrors.rolesToUser}</p>
+              ) : (
+                ""
+              ))}
             <input type="submit" onClick={handleSubmit}></input>
           </div>
         </form>
-        
       </div>
     </div>
   );
